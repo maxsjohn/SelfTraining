2f12ef3ddd2d9744537b439de8dd6db9729d10ba
maxsjohn <maxsjohn@gmail.com>
2016-12-17 11:13:17 +0530

    Update Queue.java

--- SelfTraining/SelfTraining/src/exercise/Queue.java
+++ SelfTraining/SelfTraining/src/exercise/Queue.java
@@ -1,22 +1,5 @@
-/*
- * Class: Queue
- *
- * Created on Dec 15, 2016
- *
- * (c) Copyright Lam Research Corporation, unpublished work, created 2016
- * All use, disclosure, and/or reproduction of this material is prohibited
- * unless authorized in writing.  All Rights Reserved.
- * Rights in this program belong to:
- * Lam Research Corporation
- * 4000 N. First Street
- * San Jose, CA
- */
-package practise;
 
-/**
- * @author ViegasJ
- *
- */
+package practise;
 public interface Queue<Item>
 {
     final int INITIAL_LENGTH = 4;

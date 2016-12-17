3456abf59499100f32d2ed4f3ecbc9db95b91ea6
maxsjohn <maxsjohn@gmail.com>
2016-12-17 11:14:12 +0530

    Update QueueArray.java

--- SelfTraining/SelfTraining/src/exercise/QueueArray.java
+++ SelfTraining/SelfTraining/src/exercise/QueueArray.java
@@ -1,16 +1,4 @@
-/*
- * Class: QueueArray
- *
- * Created on Dec 15, 2016
- *
- * (c) Copyright Lam Research Corporation, unpublished work, created 2016
- * All use, disclosure, and/or reproduction of this material is prohibited
- * unless authorized in writing.  All Rights Reserved.
- * Rights in this program belong to:
- * Lam Research Corporation
- * 4000 N. First Street
- * San Jose, CA
- */
+
 package practise;
 
 /**
